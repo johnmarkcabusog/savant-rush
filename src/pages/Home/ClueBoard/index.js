09639cf5cc668991c5747b95114a3efc7b980ebb
jmcabusog <johnmarkcabusog@gmail.com>
2022-07-24 21:23:23 +0800

    Clue Board updates

--- src/pages/Home/ClueBoard/index.js
+++ src/pages/Home/ClueBoard/index.js
@@ -2,8 +2,7 @@ import React, { Fragment } from "react";
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ClueBoard = ({ wordsToGuess, gameOver }) => {
-  console.log("WORRDSD  TO GUESS", wordsToGuess)
+const ClueBoard = ({ wordsToGuess, gameOver, setTheWordsToGuess }) => {
   return (
     <Fragment>
       <div className="clue-header">Word Clues</div>
@@ -15,13 +14,23 @@ const ClueBoard = ({ wordsToGuess, gameOver }) => {
           ) : (
             <div className="word-name">{words.word.toUpperCase()}</div>
           )}
-          <div className="show-clue-but"><span> Show Clue <FontAwesomeIcon icon={`eye`} /></span></div>
+          <div className="show-clue-but"><span onClick={()=>{
+            const clonedWordsToGuess = [...wordsToGuess];
+            const foundIndex = clonedWordsToGuess.findIndex(x=> x.word === words.word);
+            clonedWordsToGuess[foundIndex].shownClue = clonedWordsToGuess[foundIndex].shownClue + 1;
+            setTheWordsToGuess(clonedWordsToGuess)
+          }}> Show Clue <FontAwesomeIcon icon={`eye`} /></span></div>
         </div>
           <div className="definition">{
             words.defs.map((w, i)=>(
               <div key={i}>{(()=>{
-                const splitWord = w.split('\t');
-                return `(${splitWord[0]}) ${splitWord[1]}`
+                if(i <= words.shownClue){
+                  const splitWord = w.split('\t');
+                  return <span className="word-def">{`(${splitWord[0]}) ${splitWord[1]}`}</span> 
+                }else{
+                  return <div className="hidden-defs"></div>
+                }
+             
               })()}</div>
             ))
           }</div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,26 +7,26 @@ import TileAnswer from "./TileAnswer";
 import Tiles from "./Tiles";
 import ClueBoard from "./ClueBoard";
 
-const massageData = data=>{
-  console.log("massageData", data)
-  let massagedData = data.map((data)=>{
-      // check if data has defintion, else don't  include
-      if(data.hasOwnProperty('defs')){
-        if(data.defs.length > 0){
-           let definition_list=[];
-           if(data.defs.length > 3){
-            // get the first 3 defintion
-            definition_list = [data.defs[0], data.defs[1], data.defs[2]]
-           }else{
-            definition_list = data.defs;
-            // get all defiintion
-           }
-           return {word:data.word, defs: definition_list, shownClue:0}
+const massageData = (data) => {
+  let massagedData = data.map((data) => {
+    // check if data has defintion, else don't  include
+    if (data.hasOwnProperty("defs")) {
+      if (data.defs.length > 0) {
+        let definition_list = [];
+        if (data.defs.length > 3) {
+          // get the first 3 defintion
+          definition_list = [data.defs[0], data.defs[1], data.defs[2]];
+        } else {
+          definition_list = data.defs;
+          // get all defiintion
         }
+        return { word: data.word, defs: definition_list, shownClue: 0 };
       }
+    }
+    return null;
   });
   return massagedData;
-}
+};
 
 const Home = () => {
   const [wordsToGuess, setTheWordsToGuess] = useState([]);
@@ -65,28 +65,30 @@ const Home = () => {
       const answerCorrect = wordsToGuess.find((x) => x.word === userGuess);
       if (answerCorrect) {
         let clonedCorrectAnswers = [...correctAnswers];
-        clonedCorrectAnswers.push({word_tile_positions: userAnswer, word: userGuess});
+        clonedCorrectAnswers.push({
+          word_tile_positions: userAnswer,
+          word: userGuess,
+        });
         setCorrectAnswers(clonedCorrectAnswers);
         setUserAnswer([]);
         removeAllSelectedClass(userAnswer);
       }
     }
-  }, [userAnswer,correctAnswers, wordsToGuess]);
+  }, [userAnswer, correctAnswers, wordsToGuess]);
 
   // only trigger tile color updates for correct answer tiles
-  useEffect(()=>{
-    correctAnswers.forEach((x, index)=>{
-      const { word_tile_positions} = x;
-      word_tile_positions.forEach(w=>{
+  useEffect(() => {
+    correctAnswers.forEach((x, index) => {
+      const { word_tile_positions } = x;
+      word_tile_positions.forEach((w) => {
         const elementTile = document.getElementsByClassName(w.tile_pos);
-        if(elementTile.length > 0){
+        if (elementTile.length > 0) {
           elementTile[0].classList.add(`correct-answer-${index}`);
         }
-      })
-    
-    })
-  },[correctAnswers])
-  
+      });
+    });
+  }, [correctAnswers]);
+
   return (
     <div className="main-panel">
       <div className="game-board">
@@ -103,7 +105,9 @@ const Home = () => {
           <TileAnswer userAnswer={userAnswer} setUserAnswer={setUserAnswer} />
         </div>
       </div>
-      <div className="clue-board"> <ClueBoard wordsToGuess={wordsToGuess} gameOver={gameOver}/></div>
+      <div className="clue-board">
+        <ClueBoard wordsToGuess={wordsToGuess} gameOver={gameOver} setTheWordsToGuess={setTheWordsToGuess} />
+      </div>
     </div>
   );
 };
